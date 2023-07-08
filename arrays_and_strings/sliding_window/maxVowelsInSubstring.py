f3b2a5ed9eb1a66f6e570d94b832c95be3546a4e
David Levy <dmldatasci@gmail.com>
2023-07-08 16:02:24 -0600

    Added python scripts for sliding window methods.

---
+++ arrays_and_strings/sliding_window/maxVowelsInSubstring.py
@@ -0,0 +1,28 @@
+"""
+1456 – Maximum Number of Vowels in a Substring of Given Length (https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/)
+
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+
+Example 1:
+Input: s = "abciiidef", k = 3
+Output: 3
+
+
+Example 2:
+Input: s = "aeiou", k = 2
+Output: 2
+
+Example 3:
+Input: s = "leetcode", k = 3
+Output: 2
+
+
+Constraints:
+1 <= s.length <= 105
+s consists of lowercase English letters.
+1 <= k <= s.length
+"""
+
