23abeb59c6f99c818926d9a73d803a6ac6e1fb43
David Levy <dmldatasci@gmail.com>
2023-07-09 07:35:03 -0600

    Implemented a method for comparing frequency counts of characters in strings using a hash map technique.

---
+++ hashing/counting/areOccurrencesEqual.py
@@ -0,0 +1,38 @@
+"""
+1941 – Check if All Characters Have Equal Number of Occurrences (https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/)
+
+Given a string s, return true if s is a good string, or false otherwise.
+
+A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+
+Example 1:
+Input: s = "abacbc"
+Output: true
+
+Example 2:
+Input: s = "aaabb"
+Output: false
+
+
+Constraints:
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+"""
+
+from collections import defaultdict
+
+def areOccurrencesEqual() -> bool:
+
+    # instantiate a hash map
+    d = defaultdict(int)
+
+    # loop through characters in the input string and increment hash map values corresponding to each key
+    for c in s:
+        d[c] += 1
+
+    # convert hash map values to a set
+    counts = set(d.values())
+
+    # there should only be one value in the set for a 'good' string
+    return len(counts) == 1
